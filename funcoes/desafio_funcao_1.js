8ff19ef65f89a5d135d3214cfb33e27076094163
Kellinton <kellintonbrasil@gmail.com>
2023-11-12 12:25:27 -0300

    desafio/funcao1

---
+++ funcoes/desafio_funcao_1.js
@@ -0,0 +1,13 @@
+// funções que recebem 2 parâmetros/argumentos
+
+function somar(a, b){
+    console.log(a + b);
+}
+
+function subtrair(c, d){
+    console.log(c - d);
+}
+
+somar(7, 3)
+subtrair(10, 3)
+
