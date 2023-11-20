c4469f9ba735a7b836a538b6f93259a15b294392
Kellinton <kellintonbrasil@gmail.com>
2023-11-20 11:33:53 -0300

    array com for

---
+++ arrays/for_1.js
@@ -0,0 +1,11 @@
+// percorrendo os elemntos de um array utilizando laço de repetição "for"
+
+// ÍNDICES       0, 1,   2, 3, 4, 5,  6,  7
+const numeros = [1, 2, 356, 4, 5, 6, 11, 45];
+             // i < tamanho do array?
+for (let i = 0; i < numeros.length; i++) {
+    console.log(i, numeros[i]);   
+}
+
+
+
