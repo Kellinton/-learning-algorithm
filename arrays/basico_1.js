ac660c41125525744fd3dd40b0744b9b5316f87a
Kellinton <kellintonbrasil@gmail.com>
2023-11-20 10:35:20 -0300

    arrays

---
+++ arrays/basico_1.js
@@ -0,0 +1,19 @@
+
+// os arrays em javascript são dinâmicos, não precisa especificar quantos índices terá
+
+
+// ÍNDICE       0     1   2    3    4    5 (São 6 elementos)
+
+const notas = [7.8, 6.7, 10, 9.5, 7.8, 5.8];
+
+// acessar todos os dados de um array
+console.log(notas);
+
+
+// acessar por índice
+console.log(notas[3]);
+console.log(notas[5]);
+
+
+// alterando o dado do índice 3
+notas[3] = 8.7;
