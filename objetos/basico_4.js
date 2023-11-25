db1f2a4d12e3b2cafc411062dcb573245fff0e3a
Kellinton <kellintonbrasil@gmail.com>
2023-11-25 15:21:37 -0300

    objeto

--- objetos/basico_4.js
+++ objetos/basico_4.js
@@ -5,7 +5,9 @@ const usuario = {
 
 usuario.nome = 'Aluno';
 usuario.senha = '12345';
+usuario.endereco = 'Rua';
 
 console.log(usuario.email);
 console.log(usuario.nome);
+console.log(usuario.endereco);
 console.log(usuario);
