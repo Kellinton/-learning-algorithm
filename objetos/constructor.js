a812cb261637b48fcf31a0f7528e897172433a09
Kellinton <kellintonbrasil@gmail.com>
2023-11-27 13:46:17 -0300

    constructor

---
+++ objetos/constructor.js
@@ -0,0 +1,11 @@
+class Data {
+    constructor(d = 1, m = 2, a = 2022) {
+        this.dia = 2
+        this.mes = 10
+        this.ano = 2023
+    }
+};
+
+const d1 = new Data();
+
+console.log(d1);
